570b0134f6dd67b1bf0873bd061193979175d190
oscar20 <oscar_almazan@outlook.com>
2018-05-12 09:48:43 -0500

    Initial Commit

---
+++ subirImagenFirebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  subirImagenFirebase
+//
+//  Created by d182_oscar_a on 12/05/18.
+//  Copyright © 2018 d182_oscar_a. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
